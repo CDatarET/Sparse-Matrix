a715cf0f1184e5e8322d7a90edd785df32e508c8
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-07-28 19:53:07 +0530

    Update SparseMatrix.c

--- SparseMatrix.c
+++ SparseMatrix.c
@@ -45,19 +45,29 @@ void compact(int rows, int cols, int arr[rows][cols], int cp[rows][3]){
     cp[0][2] = k - 1;
 }
 
-void transpose(int rows, int cp[rows][3], int s[rows][3]){
+void transpose(int cp[][3], int t[][3]){
     int m = cp[0][0];
     int n = cp[0][1];
-    int t = cp[0][2];
+    int e = cp[0][2];
+    t[0][0] = m;
+    t[0][1] = n;
+    t[0][2] = e;
 
-    if(t <= 0){
+    if(e <= 0){
         return;
     }
-    
-    for(int col = 0; col < n; col++){
 
+    int q = 1;
+    for(int i = 0; i < n; i++){
+        for(int j = 1; j <= e; j++){
+            if(cp[j][1] == i){
+                t[q][0] = cp[j][1];
+                t[q][1] = cp[j][0];
+                t[q][2] = cp[j][2];
+                q++;
+            }
+        }
     }
-
 }
 
 int main(){
@@ -79,10 +89,10 @@ int main(){
     compact(rows, cols, a, cp);
     disp(cp[0][2] + 1, 3, cp);
 
-    int t[cp[0][2] + 1][3];
-    transpose(cp[0][2] + 1, cp, t);
-
+    int nz = cp[0][2];
+    int t[nz + 1][3];
+    transpose(cp, t);
     disp(t[0][2] + 1, 3, t);
-
-    return(0);
+    
+    return(0);
 }
